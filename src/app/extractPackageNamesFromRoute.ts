e08768075bfe5302c032aecdb0f2173b4c2d50f5
Yves Bonjour <yves.bonjour@rethinc.ch>
2022-02-25 16:32:52 +0100

    Ensures only unique packagenames are used

--- src/app/extractPackageNamesFromRoute.ts
+++ src/app/extractPackageNamesFromRoute.ts
@@ -6,9 +6,13 @@ export const extractPackageNamesFromRoute = (
   const packageNames = route.query['packagename']
 
   if (typeof packageNames === 'object') {
-    return packageNames as string[]
+    return (packageNames as string[]).filter(onlyUnique)
   } else if (typeof packageNames === 'string') {
     return [packageNames]
   }
   return []
+}
+
+const onlyUnique = (value: string, index: number, self: string[]) => {
+  return self.indexOf(value) === index
 }
