940f6a3b85a4b4fa70c5e74902278ed526a813b6
Yves Bonjour <yves.bonjour@rethinc.ch>
2022-02-25 15:33:17 +0100

    Adds AppCArd to examples

--- view-examples/examples.ts
+++ view-examples/examples.ts
@@ -1,6 +1,7 @@
 import { Component } from 'vue'
 import IconViewExample from './examples/shared/IconViewExample.vue'
 import ShareUrlExample from './examples/components/ShareUrlExample.vue'
+import AppCardExample from './examples/components/AppCardExample.vue'
 
 export interface Example {
   name: string
@@ -9,5 +10,11 @@ export interface Example {
 
 export const examples = new Map<string, Example[]>([
   ['Shared', [{ name: 'Icons', component: IconViewExample }]],
-  ['Components', [{ name: 'ShareUrl', component: ShareUrlExample }]],
+  [
+    'Components',
+    [
+      { name: 'ShareUrl', component: ShareUrlExample },
+      { name: 'AppCard', component: AppCardExample },
+    ],
+  ],
 ])
---
+++ view-examples/examples/components/AppCardExample.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>App Card component</h1>
+
+  <h2>Basic state</h2>
+  <div>
+    <app-card package-name="ch.viavelo.customer" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import AppCard from '../../../src/components/AppCard.vue'
+
+export default defineComponent({
+  name: 'AppCardExample',
+  components: { AppCard },
+})
+</script>
