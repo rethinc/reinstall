d3d2c3589f2d23d35e507474cf31a43f91b9b7cd
Christian Feuz <christian.feuz@rethinc.ch>
2022-02-25 14:48:02 +0100

    Adds ShareUrl component

---
+++ src/components/ShareUrl.vue
@@ -0,0 +1,17 @@
+<template>
+  <input v-model="shareUrl" type="text" />
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const shareUrl = ref<string>('')
+
+    return {
+      shareUrl,
+    }
+  },
+})
+</script>
