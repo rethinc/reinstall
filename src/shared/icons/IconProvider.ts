df3337a81430538beb1075dc89f2becd41d5c7da
Christian Feuz <christian.feuz@rethinc.ch>
2022-02-25 18:14:02 +0100

    Adds trash and edit icon

--- src/shared/icons/IconProvider.ts
+++ src/shared/icons/IconProvider.ts
@@ -1,12 +1,12 @@
-import download from '@/assets/icons/colorizable/download.svg?raw'
-import reinstall_share from '@/assets/icons/colorizable/reinstall_share.svg?raw'
+import edit from '@/assets/icons/colorizable/edit.svg?raw'
+import trash from '@/assets/icons/colorizable/trash.svg?raw'
 
 import reinstall_logo from '@/assets/icons/regular/reinstall_logo.svg?raw'
 import reinstall_sign from '@/assets/icons/regular/reinstall_sign.svg?raw'
 
 export enum IconColorizable {
-  Download = 'IconColorizable_Download',
-  Reinstall_share = 'IconColorizable_Reinstall_share',
+  Edit = 'IconColorizable_Edit',
+  Trash = 'IconColorizable_Trash',
 }
 
 export enum IconRegular {
@@ -18,11 +18,11 @@ export type IconType = IconColorizable | IconRegular
 
 export const iconByType = (iconType: IconType): string => {
   switch (iconType) {
-    case IconColorizable.Download:
-      return download
+    case IconColorizable.Edit:
+      return edit
 
-    case IconColorizable.Reinstall_share:
-      return reinstall_share
+    case IconColorizable.Trash:
+      return trash
 
     case IconRegular.Reinstall_logo:
       return reinstall_logo
