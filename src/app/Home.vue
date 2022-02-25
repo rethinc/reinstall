723a9abbab046920c6194792569fc832e1851658
Yves Bonjour <yves.bonjour@rethinc.ch>
2022-02-25 15:08:06 +0100

    Extracts AppCard

--- src/app/Home.vue
+++ src/app/Home.vue
@@ -3,13 +3,7 @@
     :type="IconRegular.Reinstall_logo"
     style="width: 200px; height: 200px"
   />
-  {{ packageName }}
-  <form @submit.prevent="install()">
-    <label for="versionCode">Version Code</label>
-    <input id="versionCode" v-model="versionCode" type="text" />
-    <button>Install</button>
-  </form>
-
+  <AppCard :package-name="packageName" />
   <AddApp />
 </template>
 
@@ -19,10 +13,11 @@ import IconView from '@/shared/icons/IconView.vue'
 import AddApp from '@/components/AddApp.vue'
 import { IconColorizable, IconRegular } from '@/shared/icons/IconProvider'
 import { RouteLocationNormalizedLoaded, useRoute } from 'vue-router'
+import AppCard from '@/components/AppCard.vue'
 
 export default defineComponent({
   name: 'App',
-  components: { IconView, AddApp },
+  components: { IconView, AddApp, AppCard },
   setup() {
     const route = useRoute()
     const extractPackageName = (
@@ -34,7 +29,6 @@ export default defineComponent({
     }
 
     const packageName = ref<string>(extractPackageName(route) ?? '')
-    const versionCode = ref<string>('1569')
 
     watch(route, () => {
       const newQueryPackage = extractPackageName(route)
@@ -43,18 +37,10 @@ export default defineComponent({
       }
     })
 
-    const install = (): void => {
-      window.open(
-        `https://play.google.com/apps/test/${packageName.value}/${versionCode.value}`
-      )
-    }
-
     return {
       IconColorizable,
       IconRegular,
       packageName,
-      versionCode,
-      install,
     }
   },
 })
---
+++ src/components/AppCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1>{{ packageName }}</h1>
+  <form @submit.prevent="install()">
+    <label for="versionCode">Version Code</label>
+    <input id="versionCode" v-model="versionCode" type="text" />
+    <button>Install</button>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, ref } from 'vue'
+
+export default defineComponent({
+  props: {
+    packageName: {
+      type: String as PropType<string>,
+      required: true,
+    },
+  },
+  setup(props) {
+    const versionCode = ref<string>('1569')
+
+    const install = (): void => {
+      window.open(
+        `https://play.google.com/apps/test/${props.packageName}/${versionCode.value}`
+      )
+    }
+
+    return {
+      versionCode,
+      install,
+    }
+  },
+})
+</script>
+
+<style scoped lang="scss">
+@import '../assets/styles/colors';
+@import '../assets/styles/browser-reset';
+
+.colorizedIcon {
+  color: $primary;
+}
+</style>
