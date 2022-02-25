8e439c6c6d98afbf53ce6f70a7b1c6610f30d649
simonvonallmen <simon.vonallmen@rethinc.ch>
2022-02-25 15:26:44 +0100

    Adds navbar and styling

--- src/app/Home.vue
+++ src/app/Home.vue
@@ -1,4 +1,10 @@
 <template>
+  <nav class="navigation-wrapper">
+    <div class="navigation-items">
+      <icon-view :type="IconRegular.Reinstall_sign" style="height: 60px" />
+    </div>
+    <span class="gradient" />
+  </nav>
   <icon-view
     :type="IconRegular.Reinstall_logo"
     style="width: 200px; height: 200px"
@@ -52,6 +58,28 @@ export default defineComponent({
 <style scoped lang="scss">
 @import '../assets/styles/colors';
 @import '../assets/styles/browser-reset';
+@import '../assets/styles/sizes-and-spacings';
+
+.navigation-wrapper {
+  position: relative;
+  padding-bottom: 40px;
+
+  .navigation-items {
+    background: $white;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    padding: $spacing-normal 0;
+  }
+
+  .gradient {
+    position: absolute;
+    bottom: 0;
+    width: 100vw;
+    height: 40px;
+    background: linear-gradient(180deg, #50e3c2 0%, rgba(80, 227, 194, 0) 100%);
+  }
+}
 
 .colorizedIcon {
   color: $primary;
