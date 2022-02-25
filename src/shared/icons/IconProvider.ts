ab07df083f223fad6587c5bef70a57bddb750dc9
Christian Feuz <christian.feuz@rethinc.ch>
2022-02-25 18:43:37 +0100

    Fixes missing icon bug

--- src/shared/icons/IconProvider.ts
+++ src/shared/icons/IconProvider.ts
@@ -1,4 +1,5 @@
 import edit from '@/assets/icons/colorizable/edit.svg?raw'
+import reinstall_share from '@/assets/icons/colorizable/reinstall_share.svg?raw'
 import trash from '@/assets/icons/colorizable/trash.svg?raw'
 
 import reinstall_logo from '@/assets/icons/regular/reinstall_logo.svg?raw'
@@ -6,6 +7,7 @@ import reinstall_sign from '@/assets/icons/regular/reinstall_sign.svg?raw'
 
 export enum IconColorizable {
   Edit = 'IconColorizable_Edit',
+  Reinstall_share = 'IconColorizable_Reinstall_share',
   Trash = 'IconColorizable_Trash',
 }
 
@@ -20,6 +22,9 @@ export const iconByType = (iconType: IconType): string => {
   switch (iconType) {
     case IconColorizable.Edit:
       return edit
+
+    case IconColorizable.Reinstall_share:
+      return reinstall_share
 
     case IconColorizable.Trash:
       return trash
