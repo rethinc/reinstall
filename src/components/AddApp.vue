216b0e291a6b4db694df4b94a3b417e103903d52
Christian Feuz <christian.feuz@rethinc.ch>
2022-02-25 13:56:35 +0100

    Adds form to add new packagename

--- src/components/AddApp.vue
+++ src/components/AddApp.vue
@@ -2,7 +2,12 @@
   <template v-if="!isAdding">
     <button @click="addApp()">+</button>
   </template>
-  <template v-if="isAdding"> Adding </template>
+  <template v-if="isAdding">
+    <form @submit.prevent="submit()">
+      <input v-model="packageName" type="text" />
+      <button>Add</button>
+    </form>
+  </template>
 </template>
 
 <script lang="ts">
@@ -12,14 +17,22 @@ export default defineComponent({
   setup() {
     const isAdding = ref<boolean>(false)
 
+    const packageName = ref<string>('')
+
     const addApp = () => {
       isAdding.value = true
     }
 
+    const submit = () => {
+      console.log('packagename', packageName.value)
+    }
+
     return {
       addApp,
+      submit,
       isAdding,
+      packageName
     }
-  },
+ },
 })
 </script>
