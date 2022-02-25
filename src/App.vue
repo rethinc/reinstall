c076be573b260f63a5fc412661a80a681d5837f2
Yves Bonjour <yves.bonjour@rethinc.ch>
2022-02-25 12:19:02 +0100

    Moves home to a component

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,16 @@
 <template>
-  <icon-view
-    :type="IconRegular.Reinstall_logo"
-    style="width: 200px; height: 200px"
-  />
-  <form @submit.prevent="install()">
-    <input v-model="packageName" type="text" />
-    <input v-model="buildNumber" type="text" />
-    <button>Install</button>
-  </form>
+  <home />
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
-import IconView from '@/shared/icons/IconView.vue'
-import { IconColorizable, IconRegular } from '@/shared/icons/IconProvider'
+import { defineComponent } from 'vue'
+import Home from '@/app/Home.vue'
 
 export default defineComponent({
   name: 'App',
-  components: { IconView },
+  components: { Home },
   setup() {
-    const packageName = ref<string>('ch.viavelo.customerapp')
-    const buildNumber = ref<string>('1569')
-    const install = (): void => {
-      window.open(
-        `https://play.google.com/apps/test/${packageName.value}/${buildNumber.value}`
-      )
-    }
-
-    return {
-      IconColorizable,
-      IconRegular,
-      packageName,
-      buildNumber,
-      install,
-    }
+    return {}
   },
 })
 </script>
-
-<style scoped lang="scss">
-@import './assets/styles/colors';
-
-.colorizedIcon {
-  color: $primary;
-}
-</style>
---
+++ src/app/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <icon-view
+    :type="IconRegular.Reinstall_logo"
+    style="width: 200px; height: 200px"
+  />
+  <form @submit.prevent="install()">
+    <input v-model="packageName" type="text" />
+    <input v-model="buildNumber" type="text" />
+    <button>Install</button>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import IconView from '@/shared/icons/IconView.vue'
+import { IconColorizable, IconRegular } from '@/shared/icons/IconProvider'
+
+export default defineComponent({
+  name: 'App',
+  components: { IconView },
+  setup() {
+    const packageName = ref<string>('ch.viavelo.customerapp')
+    const buildNumber = ref<string>('1569')
+    const install = (): void => {
+      window.open(
+        `https://play.google.com/apps/test/${packageName.value}/${buildNumber.value}`
+      )
+    }
+
+    return {
+      IconColorizable,
+      IconRegular,
+      packageName,
+      buildNumber,
+      install,
+    }
+  },
+})
+</script>
+
+<style scoped lang="scss">
+@import '../assets/styles/colors';
+
+.colorizedIcon {
+  color: $primary;
+}
+</style>
