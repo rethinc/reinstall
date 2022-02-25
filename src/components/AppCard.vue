c8942c187ffe8cd688e36b01f85ec485f8e0c4d4
Yves Bonjour <yves.bonjour@rethinc.ch>
2022-02-25 15:15:18 +0100

    Integrates Share functionality

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -5,6 +5,8 @@
     <input id="versionCode" v-model="versionCode" type="text" />
     <button>Install</button>
   </form>
+  <button @click="share()">Share</button>
+  <div v-if="copyUrl">{{ copyUrl }}</div>
 </template>
 
 <script lang="ts">
@@ -19,16 +21,22 @@ export default defineComponent({
   },
   setup(props) {
     const versionCode = ref<string>('1569')
+    const copyUrl = ref<string | undefined>(undefined)
 
     const install = (): void => {
       window.open(
         `https://play.google.com/apps/test/${props.packageName}/${versionCode.value}`
       )
     }
+    const share = () => {
+      copyUrl.value = window.location.href
+    }
 
     return {
       versionCode,
+      copyUrl,
       install,
+      share,
     }
   },
 })
