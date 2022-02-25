e668b1260d530645b3c89de7a01d00f4377eb9d1
Christian Feuz <christian.feuz@rethinc.ch>
2022-02-25 18:23:43 +0100

    Fixes AppButtonExample

--- view-examples/examples/shared/AppButtonExample.vue
+++ view-examples/examples/shared/AppButtonExample.vue
@@ -3,26 +3,17 @@
   <p>Relative to font icon size</p>
   <AppButton>Click me</AppButton>
   <AppButton>
-    <IconView
-      :type="IconColorizable.Download"
-      style="width: 24px; height: 24px"
-    />
+    <IconView :type="IconColorizable.Edit" style="width: 24px; height: 24px" />
   </AppButton>
   <p>Fixed button width</p>
   <AppButton style="width: 300px">Click me</AppButton>
   <AppButton style="width: 300px">
-    <IconView
-      :type="IconColorizable.Download"
-      style="width: 24px; height: 24px"
-    />
+    <IconView :type="IconColorizable.Edit" style="width: 24px; height: 24px" />
   </AppButton>
   <p>Button appearance transparent</p>
   <AppButton :appearance="AppButtonAppearance.Transparent">Click me</AppButton>
   <AppButton :appearance="AppButtonAppearance.Transparent">
-    <IconView
-      :type="IconColorizable.Download"
-      style="width: 24px; height: 24px"
-    />
+    <IconView :type="IconColorizable.Edit" style="width: 24px; height: 24px" />
   </AppButton>
 </template>
 
