88014e45554b8da763418214c9358e8087848a2e
Chris Weber <chris.weber@rethinc.ch>
2022-03-18 11:11:30 +0100

    Separates iframe content app from overview app because global styles could not be isolated

--- viewExamples/viewExampleIFrame/App.vue
+++ viewExamples/viewExampleIFrame/App.vue
@@ -6,13 +6,13 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'ViewExample',
+  name: 'App',
 })
 </script>
 
 <style lang="scss">
-@import '../../../src/assets/styles/browser-reset';
-@import '../../../src/assets/styles/colors';
+@import '../../src/assets/styles/browser-reset';
+@import '../../src/assets/styles/colors';
 
 body {
   background: $accent;
